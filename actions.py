b3123db614abb68b5c1619abd8778af550a2f68b
divyadavis <48674739+divyadavis@users.noreply.github.com>
2020-03-13 18:19:02 +0530

    uploaded on 13 march 2020

--- actions.py
+++ actions.py
@@ -1,4 +1,5 @@
 from typing import Any, Text, Dict, List
+import requests, json
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
@@ -17,6 +18,9 @@ class ActionShow(Action):
         #tmp = tracker.get_slot("department_name")
         
         tmp = next(tracker.get_latest_entity_values("department_name"), None)
+
+        print("tmp", tmp)
+
         dispatcher.utter_message(text="you want to know something related to {}".format(tmp))
         #print(tmp)
         return
@@ -43,6 +47,47 @@ class ActionLocation(Action):
         return 
 
        
+
+
+
+
+class ActionWeather(Action):
+     def name(self) -> Text:
+        return "action_weather"
+     def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+         print("action file called : action_weather")
+        
+
+         location = next(tracker.get_latest_entity_values("city_name"), None)
+
+         print("location", location)
+
+         url = "http://api.openweathermap.org/data/2.5/weather?q=" + "Bengaluru" + "&APPID=3a1460156ae6d16dc1fa7cc8e986e94e"
+
+         print("url :", url)
+
+         response = requests.get(url)
+
+         #print(response)
+
+         data = response.json()
+
+         #print(data["cod"])
+
+         r = data["main"]
+
+         #print("temperature:", r["temp"])
+         #print("pressure:", r["pressure"])
+         #print("humidity:", r["humidity"])
+
+
+         response = """ The temperature is {} , The pressure is {}, The humidity is {} %.""".format(r["temp"], r["pressure"], r["humidity"])
+        
+         dispatcher.utter_message(response)
+
 
 
 
